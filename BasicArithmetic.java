48baf240b70f950bffcd8c1c559c0e0ecb8731b4
JPOliverio <88540433+JPOliverio@users.noreply.github.com>
2021-09-20 11:13:21 -0400

    Create BasicArithmetic.java

---
+++ BasicArithmetic.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+
+public class BasicArithmetic{
+
+  public basicarithmetic(){
+  }
+  
+  addition
+    
+  subtraction
+    
+  division
+  
+  squareRoot
+    
+  exponent
+    
+  toOctal
+    
+  toHex
+    
+  toInt
+    
+  toBinary
+    
+  
+  
+  
+  
+}
